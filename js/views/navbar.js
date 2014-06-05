346217e160affbba82734dbef835620af7939e6d
arik <lewin.arik@gmail.com>
2014-06-05 14:33:25 +0300

    dynamic navigation bullets refactor

--- js/views/navbar.js
+++ js/views/navbar.js
@@ -19,8 +19,30 @@ define([
         initialize: function () {
             var me = this;
 
-            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'change', this.update);
             this.model.set({"counter": 4});
+            this.render();
+
+        },
+
+        update: function() {
+            var children =  $(this.el).children(),
+                me = this;
+
+            if (children) {
+                children.each(function() {
+                    var elt = $(this),
+                        lielt,
+                        id = elt.prop("id"),
+                        currentPage = (me.model.get("id") || 1);
+
+                    lielt = elt.find("li");
+                    lielt.removeClass("active");
+                    if (id.charAt(id.length-1) === currentPage) {
+                        lielt.addClass("active");
+                    }
+                })
+            }
         },
 
         // Re-render
@@ -30,6 +52,7 @@ define([
                 out = "",
                 currentPage,
                 arr = [], idx = 1;
+
 
             counter = (this.model.get("counter") || 0);
             currentPage = (this.model.get("id") || 1);
